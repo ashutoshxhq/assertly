daedf4c37a0a65986023c120f25ba81c1cf4f161
Ashutosh Dubey <ashutosh@richpanel.com>
2024-07-14 15:11:59 +0530

    updated health endpoints

--- apps/engine-service/src/health/health.controller.ts
+++ apps/engine-service/src/health/health.controller.ts
@@ -1,12 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
 
-@Controller('health')
+@Controller('')
 export class HealthController {
-    @Get('v1/health')
-    async health() {
-        return {
-            status: 'UP',
-            version: '1.0.0',
-        };
-    }
+  @Get('v1/health')
+  async health() {
+    return {
+      status: 'UP',
+      version: '1.0.0',
+    };
+  }
 }

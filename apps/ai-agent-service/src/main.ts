4b3a7e59e186ec97bb46e8b39dc4c873748c8b45
Ashutosh Dubey <ashutosh@richpanel.com>
2024-07-14 21:32:11 +0530

    updated port env

--- apps/ai-agent-service/src/main.ts
+++ apps/ai-agent-service/src/main.ts
@@ -6,6 +6,6 @@ async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     app.enableCors();
     app.use(json({ limit: '50mb' }));
-    await app.listen(8000);
+    await app.listen(process.env.PORT || 8000);
 }
 bootstrap();
--- apps/webdriver-service/src/main.ts
+++ apps/webdriver-service/src/main.ts
@@ -5,6 +5,6 @@ import { WsAdapter } from '@nestjs/platform-ws';
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     app.useWebSocketAdapter(new WsAdapter(app));
-    await app.listen(8000);
+    await app.listen(process.env.PORT || 8000);
 }
 bootstrap();

a7db645107fe556d7692854ffe4d239e6259b21b
Ashutosh Dubey <ashutosh@richpanel.com>
2024-07-21 23:19:25 +0530

    updated settings ui

--- apps/assertly-app/src/pages/Settings/Account/Account.tsx
+++ apps/assertly-app/src/pages/Settings/Account/Account.tsx
@@ -2,66 +2,24 @@ import {
     Card,
     CardHeader,
     CardTitle,
-    CardDescription,
     CardContent,
     CardFooter,
 } from "src/components/ui/card";
 
-import { Input } from "src/components/ui/input";
-import { Label } from "src/components/ui/label";
 import { Button } from "src/components/ui/button";
-import { CopyIcon } from "@radix-ui/react-icons";
 
 const Account = () => {
     return (
         <div>
             <Card className="max-w-6xl">
                 <CardHeader>
-                    <CardTitle>Account Settings</CardTitle>
+                    <CardTitle>User Profile</CardTitle>
                 </CardHeader>
                 <CardContent>
-                    <form>
-                        <div className="grid w-full items-center gap-4">
-                            <div className="flex flex-col space-y-1.5">
-                                <Label htmlFor="orgId">Organization ID</Label>
-                                <div className="flex gap-1 items-center">
-                                    <Input
-                                        id="orgId"
-                                        value="0c691884-3204-451c-8020-d0d3ffbaf97a"
-                                        disabled
-                                    />
-                                    <Button
-                                        size="icon"
-                                        variant="outline"
-                                        className="h-8 w-8 opacity-100"
-                                    >
-                                        <CopyIcon className="h-4 w-4" />
-                                    </Button>
-                                </div>
-                            </div>
-                            <div className="flex flex-col space-y-1.5">
-                                <Label htmlFor="orgName">
-                                    Organization Name
-                                </Label>
-                                <Input
-                                    id="orgName"
-                                    placeholder="Name of your organization"
-                                />
-                            </div>
-                            <div className="flex flex-col space-y-1.5">
-                                <Label htmlFor="primaryEmaiId">
-                                    Primary email address
-                                </Label>
-                                <Input
-                                    id="primaryEmailId"
-                                    placeholder="Primary email address"
-                                />
-                            </div>
-                        </div>
-                    </form>
+                    <form>{/* user profile update form here */}</form>
                 </CardContent>
                 <CardFooter className="flex justify-between">
-                    <Button variant={"default"}>Update Settings</Button>
+                    <Button variant={"default"}>Update Profile</Button>
                 </CardFooter>
             </Card>
         </div>
--- apps/assertly-app/src/pages/Settings/Organization/Organization.tsx
+++ apps/assertly-app/src/pages/Settings/Organization/Organization.tsx
@@ -1,5 +1,77 @@
+import {
+    Card,
+    CardHeader,
+    CardTitle,
+    CardContent,
+    CardFooter,
+} from "src/components/ui/card";
+
+import { Input } from "src/components/ui/input";
+import { Label } from "src/components/ui/label";
+import { Button } from "src/components/ui/button";
+import { CopyIcon } from "@radix-ui/react-icons";
+
 const Organization = () => {
-    return <div>Organization</div>;
+    return (
+        <div className="flex flex-col gap-4">
+            <Card className="max-w-6xl">
+                <CardHeader>
+                    <CardTitle>Organization Settings</CardTitle>
+                </CardHeader>
+                <CardContent>
+                    <form>
+                        <div className="grid w-full items-center gap-4">
+                            <div className="flex flex-col space-y-1.5">
+                                <Label htmlFor="orgId">Organization ID</Label>
+                                <div className="flex gap-1 items-center">
+                                    <Input
+                                        id="orgId"
+                                        value="0c691884-3204-451c-8020-d0d3ffbaf97a"
+                                        disabled
+                                    />
+                                    <Button
+                                        size="icon"
+                                        variant="outline"
+                                        className="h-8 w-8 opacity-100"
+                                    >
+                                        <CopyIcon className="h-4 w-4" />
+                                    </Button>
+                                </div>
+                            </div>
+                            <div className="flex flex-col space-y-1.5">
+                                <Label htmlFor="orgName">
+                                    Organization Name
+                                </Label>
+                                <Input
+                                    id="orgName"
+                                    placeholder="Name of your organization"
+                                />
+                            </div>
+                            <div className="flex flex-col space-y-1.5">
+                                <Label htmlFor="primaryEmaiId">
+                                    Primary email address
+                                </Label>
+                                <Input
+                                    id="primaryEmailId"
+                                    placeholder="Primary email address"
+                                />
+                            </div>
+                        </div>
+                    </form>
+                </CardContent>
+                <CardFooter className="flex justify-between">
+                    <Button variant={"default"}>Update Settings</Button>
+                </CardFooter>
+            </Card>
+
+            <Card className="max-w-6xl">
+                <CardHeader>
+                    <CardTitle>Team Members</CardTitle>
+                </CardHeader>
+                <CardContent>{/* table here */}</CardContent>
+            </Card>
+        </div>
+    );
 };
 
 export default Organization;

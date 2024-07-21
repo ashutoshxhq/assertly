8c6c2bca093c3b7fe2f516b9329c6d94c648b9ca
ashok-kollipara <93081449+ashok-kollipara@users.noreply.github.com>
2024-07-21 15:53:07 +0530

    change layout

--- apps/assertly-app/src/pages/Settings/SettingsLayout.tsx
+++ apps/assertly-app/src/pages/Settings/SettingsLayout.tsx
@@ -29,39 +29,35 @@ const SettingsLayout = () => {
                 </div>
             </div>
         </div>
-        <div className="border-b dark:border-zinc-900 my-8 mx-2"></div>
-        <div className="bg-zinc-50 dark:bg-zinc-800/50 border border-zinc-200 dark:border-zinc-800 rounded-md py-4 px-4">
-            <div className="flex flex-1 p-2 h-full grow rounded-lg shadow-sm ring-1 ring-zinc-200 dark:ring-zinc-950/4 bg-zinc-50 dark:bg-zinc-950 dark:ring-white/5 dark:text-zinc-300 overflow-y-scroll">
-                <Tabs
-                    defaultValue="account"
-                    className="w-[400px]"
-                >
-                    <TabsList>
-                        <TabsTrigger value="account">
-                            <MdManageAccounts className="mr-2"/>
-                            Account
-                        </TabsTrigger>
-                        <TabsTrigger value="integrations">
-                            <GoWorkflow className="mr-2"/>
-                            Integrations
-                        </TabsTrigger>
-                        <TabsTrigger value="teamMembers">
-                            <RiTeamFill className="mr-2"/>
-                            Team Members
-                        </TabsTrigger>
-                    </TabsList>
-                    <TabsContent value="account">
-                       <Account />
-                    </TabsContent>
-                    <TabsContent value="integrations">
-                        <Integrations />
-                    </TabsContent>
-                    <TabsContent value="teamMembers">
-                        <TeamMembers />
-                    </TabsContent>
-                </Tabs>
-            </div>
-        </div>
+            <Tabs
+                defaultValue="account"
+                className="w-full p-4"
+            >
+                <TabsList className="bg-gray-200">
+                    <TabsTrigger value="account">
+                        <MdManageAccounts className="mr-2"/>
+                        Account
+                    </TabsTrigger>
+                    <TabsTrigger value="integrations">
+                        <GoWorkflow className="mr-2"/>
+                        Integrations
+                    </TabsTrigger>
+                    <TabsTrigger value="teamMembers">
+                        <RiTeamFill className="mr-2"/>
+                        Team Members
+                    </TabsTrigger>
+                </TabsList>
+                <div className="border-b-2 dark:border-zinc-900 my-4 mx-2"></div>
+                <TabsContent value="account">
+                    <Account />
+                </TabsContent>
+                <TabsContent value="integrations">
+                    <Integrations />
+                </TabsContent>
+                <TabsContent value="teamMembers">
+                    <TeamMembers />
+                </TabsContent>
+            </Tabs>
     </div>
   )
 }

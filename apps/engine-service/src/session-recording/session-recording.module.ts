00a911b1c4db0338a04ca149600a8cb6ff86b121
Ashutosh Dubey <ashutoshxhq@gmail.com>
2024-08-28 07:56:21 +0530

    fixed engine service deps

--- apps/engine-service/src/session-recording/session-recording.module.ts
+++ apps/engine-service/src/session-recording/session-recording.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { SessionRecordingService } from './session-recording.service';
 import { SessionRecordingController } from './session-recording.controller';
+import { PrismaService } from 'src/common/prisma/prisma.service';
 
 @Module({
   controllers: [SessionRecordingController],
-  providers: [SessionRecordingService],
+  providers: [SessionRecordingService, PrismaService],
 })
 export class SessionRecordingModule {}
